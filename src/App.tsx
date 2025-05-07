93bef6522cafa0e3abde0a2eda0ed6e49ca72294
Theodore Abitbol <theodore.abitbol@gmail.com>
2025-05-07 13:15:55 +0200

    added custom favicon

--- src/App.tsx
+++ src/App.tsx
@@ -6,11 +6,9 @@ function App() {
     <main>
       <img className="w-full h-screen object-cover" src={riotfire} />
       <div className="absolute inset-0 bg-black opacity-50"></div>
-      <div className="absolute inset-0 flex flex-col justify-center items-center text-white">
-        <h2 className="text-4xl font-bold press-start-2p-regular mb-12">
-          RIOT SIMULATOR
-        </h2>
-        <button>Store</button>
+      <div className="absolute press-start-2p-regular inset-0 flex flex-col justify-center items-center text-white">
+        <h2 className="text-4xl font-bold mb-12">RIOT SIMULATOR</h2>
+        <button>Download</button>
         <button>Instagram</button>
         <button>Twitter</button>
         <button>TikTok</button>
